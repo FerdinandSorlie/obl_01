4f2c5c2afa370906f12286313a3a9665202380ca
FerdinandSorlie <ferdinandaugust.sorlie@outlook.com>
2021-09-07 08:59:34 +0200

    fikset skatten

--- obl_oppgaver/src/no/hvl/dat100/Skatt.java
+++ obl_oppgaver/src/no/hvl/dat100/Skatt.java
@@ -5,30 +5,21 @@ import static javax.swing.JOptionPane.showInputDialog;
 public class Skatt {
 
 	public static void main(String[]args) {
-		int lonn = Integer.parseInt(showInputDialog("lønn"));
-		
-		double[][] sats = 
+		int lonn = Integer.parseInt(showInputDialog("lï¿½nn"));
+
+		double[][] sats =
 			{
 					{ 0.0, 0, 164100},
 					{ 0.093, 164101, 230950},
-					{0.241, 230951, 580650}, 
-					{0.1152, 580651, 934050}, 
-					{0.1452, 934051, lonn 
+					{0.241, 230951, 580650},
+					{0.1152, 580651, 934050},
+					{0.1452, 934051, lonn
 			}};
-			
+
 		for (int i = 0; i< sats.length; i++) {
 			if(lonn>= sats[i][1] && lonn<= sats[i][2]) {
-			System.out.println((1-sats[i][0])*lonn + " i lønn");
+			System.out.println((lonn - (1-sats[i][0])*lonn) + " i skatt");
 			}
-		}		
+		}
 	}
 };
-	
-	
-	
-	
-	
-	
-	
-	
-
